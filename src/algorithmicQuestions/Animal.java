9297958bb3f9defed6068f73737bda1a5a70acf7
sabiaydin <sabinamammadova871@gmail.com>
2024-04-19 13:55:34 +0400

    new

---
+++ src/algorithmicQuestions/Animal.java
@@ -0,0 +1,11 @@
+package algorithmicQuestions;
+
+public class Animal<T> {
+    T sth;
+   public Animal(T sth){
+       this.sth=sth;
+   }
+    public T getSth() {
+        return sth;
+    }
+}
--- src/algorithmicQuestions/Main.java
+++ src/algorithmicQuestions/Main.java
@@ -3,12 +3,15 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Methods.isPalindrome(1223));
 
-        System.out.println(Methods.isBalanced("((({]]))"));
-        System.out.println(RomanNumber.convertRomanToDecimal("IV"));
-        int[] arr = {2, 5, 3, 7, 6, 9, 10,11};
-        Set<Integer> indexes=Methods.findIndexes(arr);
-      Methods.displayIndexesWithSum(indexes,arr);
+        Animal <String> animal = new Animal<>("Kitty");
+        System.out.println(animal.getSth());
+//        System.out.println(Methods.isPalindrome(1223));
+//
+//        System.out.println(Methods.isBalanced("((({]]))"));
+//        System.out.println(RomanNumber.convertRomanToDecimal("IV"));
+//        int[] arr = {2, 5, 3, 7, 6, 9, 10,11};
+//        Set<Integer> indexes=Methods.findIndexes(arr);
+//      Methods.displayIndexesWithSum(indexes,arr);
     }
     }
